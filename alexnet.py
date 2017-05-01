a72abc3ce683ab2d1ae61033db19355d7d31321b
Aditi Singh <787aditi@gmail.com>
2017-04-30 23:53:56 -0500

    fit alexnet

--- alexnet.py
+++ alexnet.py
@@ -38,5 +38,8 @@ network = regression(network, optimizer='momentum',
 
 # Training
 model = tflearn.DNN(network, checkpoint_path='model_alexnet',max_checkpoints=1, tensorboard_verbose=2)
+model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
+          show_metric=True, batch_size=64, snapshot_step=200,
+          snapshot_epoch=False, run_id='training')
 
 
