c1be0f25c798a33b8b94cf0bd8ad6e8796c6d008
Aditi Singh <787aditi@gmail.com>
2017-05-03 17:59:06 -0500

    fixed file type errors

--- alexnet.py
+++ alexnet.py
@@ -11,9 +11,10 @@ from tflearn.datasets import cifar10
 from sklearn.model_selection import train_test_split
 
 f=sio.loadmat('features.mat')
+t=sio.loadmat('targets.mat')
 X=f['features']
-Y=np.load('targets.npy')
-(X_train, Y_train), X_test, Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
+Y=t['targets']
+X_train, Y_train, X_test, Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
 X_train, Y_train = shuffle(X_train, Y_train)
 Y_train = to_categorical(Y_train, 8)
 Y_test = to_categorical(Y_test, 8)
