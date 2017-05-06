e4b82186bca543f049776690de6c6669cadb746a
Aditi Singh <787aditi@gmail.com>
2017-05-05 20:04:09 -0500

    updated

--- vgg16_cv.py
+++ vgg16_cv.py
@@ -1,13 +1,3 @@
-########################################################################################
-# Davi Frossard, 2016                                                                  #
-# VGG16 implementation in TensorFlow                                                   #
-# Details:                                                                             #
-# http://www.cs.toronto.edu/~frossard/post/vgg16/                                      #
-#                                                                                      #
-# Model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md     #
-# Weights from Caffe converted using https://github.com/ethereon/caffe-tensorflow      #
-########################################################################################
-
 import tensorflow as tf
 import numpy as np
 from scipy.misc import imread, imresize
